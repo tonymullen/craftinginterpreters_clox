7c0b70383e83cc879ed7079198469bbcfe32d7fc
Tony Mullen <tony.mullen@gmail.com>
2022-05-22 19:47:26 -0700

    Complete through 15.1.1

--- vm.c
+++ vm.c
@@ -1,3 +1,5 @@
+#include <stdiio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -13,10 +15,18 @@ void freeVM() {
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT()
+  (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        printValue(constant);
+        printf("\n");
+        break;
+      }
       case OP_RETURN: {
         return INTERPRET_OK;
       }
@@ -24,6 +34,7 @@ static InterpretResult run() {
   }
 
 #undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk* chunk) {
