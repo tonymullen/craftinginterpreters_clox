c9e46bfb50e51a9b1fde92d98f6f9f6cc5fe5410
Tony Mullen <tony.mullen@gmail.com>
2022-06-02 09:39:03 +0900

    Complete section 16.3

--- compiler.c
+++ compiler.c
@@ -4,4 +4,19 @@
 
 void compile(const char* source) {
   initScanner(source);
+
+  int line = -1;
+  for (;;) {
+    Token token = scanToken();
+    if (token.line != line) {
+      printf("%4d ", token.line);
+      line = token.line;
+    } else {
+      printf("   | ");
+    }
+    printf("%3d '%.*s'\n", token.type, token.length, token.start);
+
+    if (token.type == TOKEN_EOF) break;
+  }
+
 }
