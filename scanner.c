7eec4dd36b284b40b15f9f93f4ce00dfa63f4024
Tony Mullen <tony.mullen@gmail.com>
2022-06-05 08:31:50 +0900

    Through 17.2 (some errors still)

--- scanner.c
+++ scanner.c
@@ -115,7 +115,7 @@ static TokenType identifierType() {
     case 'f':
       if (scanner.current - scanner.start > 1) {
         switch (scanner.start[1]) {
-          case 'a': return checkKeyword(2, 3, 'lse', TOKEN_FALSE);
+          case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
           case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
           case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
         }
